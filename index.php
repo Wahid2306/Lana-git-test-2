b494043e7cfd21cb705583753b067dd8176b2521
M. Wahid Maulana <wahidmaulana236@gmail.com>
2023-01-05 14:07:44 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Yo karepmu </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat pariatur magnam soluta, nostrum natus sunt fuga rem neque vitae odit facilis voluptas distinctio consequuntur nihil impedit culpa ullam ipsam mollitia?</p>
+    <ul>
+        <li>one</li>
+        <li>two</li>
+    </ul>
 </body>
 </html>
